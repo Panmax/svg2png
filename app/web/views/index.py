f2ae25a086df59f32aafb27a4588a80246293dda
Panmax <jiapan.china@gmail.com>
2019-10-14 22:55:22 +0800

    html2png

--- app/web/views/index.py
+++ app/web/views/index.py
@@ -4,16 +4,23 @@ from __future__ import unicode_literals, absolute_import
 
 import io
 import cairosvg
+import imgkit
 
 from flask import Blueprint, request, send_file
-
 
 
 bp = Blueprint('home', __name__)
 
 
-@bp.route('/convert', methods = ['POST'])
+@bp.route('/convert', methods=['POST'])
 def index():
     data = request.data
     ret = cairosvg.svg2png(bytestring=data)
+    return send_file(io.BytesIO(ret), mimetype='image/png')
+
+
+@bp.route('/html/convert', methods=['POST'])
+def index():
+    data = request.data
+    ret = imgkit.from_string(data, False)
     return send_file(io.BytesIO(ret), mimetype='image/png')
